4877d03b760995d9ac721608503878f017d6ed30
Pablo Souza <pablo.osouza@outlook.com>
2024-06-07 10:25:14 -0300

    feat: configuring basic documentation

---
+++ Dima.Api/Program.cs
@@ -0,0 +1,49 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.CustomSchemaIds(n => n.FullName);
+});
+builder.Services.AddTransient<Hanlder>();
+var app = builder.Build();
+
+app.MapPost(
+        "/v1/transactions",
+        (Request request, Hanlder handler)
+        => handler.Handle(request))
+    .WithName("Transactions: Create")
+    .Produces<Response>();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.Run();
+
+public class Request
+{
+    public string Title { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public int Type { get; set; }
+    public decimal Amount { get; set; }
+    public long CategoryId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+}
+
+public class Response
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+}
+
+public class Hanlder
+{
+    public Response Handle(Request request)
+    {
+        return new Response
+        {
+            Id = 1,
+            Title = request.Title
+        };
+    }
+}
