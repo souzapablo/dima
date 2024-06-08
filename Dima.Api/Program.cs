fcbf022a7e074a303af110d4475642f805b33d54
Pablo Souza <pablo.osouza@outlook.com>
2024-06-08 11:05:05 -0300

    feat: configure identity

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,7 +1,9 @@
 using Dima.Api.Data;
 using Dima.Api.Endpoints;
 using Dima.Api.Handlers;
+using Dima.Api.Models;
 using Dima.Core.Handlers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,24 +11,39 @@ var connectionString = builder
     .Configuration
     .GetConnectionString("DefaultConnection") ?? string.Empty;
 
+builder.Services
+    .AddAuthentication(IdentityConstants.ApplicationScheme)
+    .AddIdentityCookies();
+builder.Services.AddAuthorization();
+
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     {
         options.UseSqlServer(connectionString);
     });
+
+builder.Services
+    .AddIdentityCore<User>()
+    .AddRoles<IdentityRole<long>>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddApiEndpoints();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(x =>
 {
     x.CustomSchemaIds(n => n.FullName);
 });
-builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
-builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
 
 var app = builder.Build();
 
 app.MapGet("/", () => new { message = "OK" });
 
 app.MapEndpoints();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseSwagger();
 app.UseSwaggerUI();
