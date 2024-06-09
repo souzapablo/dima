30b4bf784dc36a55ef90d578106d7f4954ceae64
Pablo Souza <pablo.osouza@outlook.com>
2024-06-09 12:09:49 -0300

    feat: configure dark mode and bind to system preference

---
+++ Dima.Web/Configuration.cs
@@ -0,0 +1,36 @@
+using MudBlazor;
+
+namespace Dima.Web;
+
+public static class Configuration
+{
+    public static MudTheme Theme { get; set; } = new MudTheme
+    {
+        Typography = new Typography
+        {
+            Default = new Default
+            {
+                FontFamily = ["Raleway", "sans-serif"]
+            }
+        },
+        Palette = new PaletteLight
+        {
+            Primary = "#1EFA2D",
+            Secondary = Colors.LightGreen.Darken3,
+            Background = Colors.Grey.Lighten4,
+            AppbarBackground = "#1EFA2D",
+            AppbarText = Colors.Shades.Black,
+            TextPrimary = Colors.Shades.Black,
+            PrimaryContrastText = Colors.Shades.Black,
+            DrawerText = Colors.Shades.Black,
+            DrawerBackground = Colors.Grey.Lighten4
+        },
+        PaletteDark = new PaletteDark
+        {
+            Primary = Colors.LightGreen.Accent3,
+            Secondary = Colors.LightGreen.Darken3,
+            AppbarBackground = Colors.LightGreen.Accent3,
+            AppbarText = Colors.Shades.Black
+        }
+    };
+}
