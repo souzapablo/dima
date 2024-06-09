8cba0e4fba350f4169bb43a35c5cc870fae233f0
Pablo Souza <pablo.osouza@outlook.com>
2024-06-09 12:16:14 -0300

    feat: add account models

---
+++ Dima.Core/Models/Account/RoleClaim.cs
@@ -0,0 +1,10 @@
+namespace Dima.Core.Models.Account;
+
+public class RoleClaim
+{
+    public string Issuer { get; set; } = string.Empty;
+    public string OrignalIssuer { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string Value { get; set; } = string.Empty;
+    public string ValueType { get; set; } = string.Empty;
+}
---
+++ Dima.Core/Models/Account/User.cs
@@ -0,0 +1,7 @@
+namespace Dima.Core.Models.Account;
+
+public class User
+{
+    public string Email { get; set; } = string.Empty;
+    public Dictionary<string,string> Claims { get; set; } = [];
+}
